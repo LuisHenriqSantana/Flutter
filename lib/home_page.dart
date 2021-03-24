a7ebe21d84ff8bc6d4e77366465c61e3c7656776
Luis <luis.henrique8000@gmail.com>
2021-03-24 18:58:03 -0300

    Animando a listas e adicionando os pokemons.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,12 +3,15 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_pokedex/consts/consts_app.dart';
 import 'package:flutter_pokedex/models/pokeapi.dart';
 import 'package:flutter_pokedex/pages/home_page/widgets/app_bar_home.dart';
+import 'package:flutter_pokedex/pages/home_page/widgets/poke_item.dart';
 import 'package:flutter_pokedex/stores/pokeapi_store.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   PokeApiStore pokeApiStore;
   @override
@@ -55,13 +58,47 @@ class _HomePageState extends State<HomePage> {
                       builder: (BuildContext context) {
                         PokeAPI _pokeApi = pokeApiStore.pokeAPI;
                         return (_pokeApi != null)
-                            ? ListView.builder(
-                          itemCount: _pokeApi.pokemon.length,
-                                itemBuilder: (context, index) {
-                                  return ListTile(
-                                    title: Text(_pokeApi.pokemon[index].name),
-                                  );
-                                },
+                            ? AnimationLimiter(
+                                child: GridView.builder(
+                                  physics: BouncingScrollPhysics(),
+                                  padding: EdgeInsets.all(12),
+                                  addAutomaticKeepAlives: true,
+                                  gridDelegate:
+                                      new SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: 2),
+                                  itemCount:
+                                      pokeApiStore.pokeAPI.pokemon.length,
+                                  itemBuilder: (context, index) {
+                                    Pokemon pokemon =
+                                        pokeApiStore.getPokemon(index: index);
+                                    return AnimationConfiguration.staggeredGrid(
+                                      position: index,
+                                      duration:
+                                          const Duration(milliseconds: 375),
+                                      columnCount: 2,
+                                      child: ScaleAnimation(
+                                        child: GestureDetector(
+                                          child: PokeItem(
+                                            types: pokemon.type,
+                                            index: index,
+                                            name: pokemon.name,
+                                            num: pokemon.num,
+                                          ),
+                                          onTap: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (BuildContext context) =>
+                                                    Container() /*PokeDetailPage(index: index)*/,
+                                                fullscreenDialog: true,
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
                               )
                             : Center(
                                 child: CircularProgressIndicator(),
--- lib/stores/pokeapi_store.dart
+++ lib/stores/pokeapi_store.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/consts/consts_api.dart';
 import 'package:flutter_pokedex/models/pokeapi.dart';
 import 'package:mobx/mobx.dart';
@@ -13,15 +15,33 @@ abstract class _PokeApiStoreBase with Store {
   @observable
   PokeAPI _pokeAPI;
 
-  @computed 
+  @computed
   PokeAPI get pokeAPI => _pokeAPI;
 
   @action
   fetchPokemonList() {
+    _pokeAPI = null;
     loadPokeAPI().then((pokeList) {
       _pokeAPI = pokeList;
     });
   }
+
+  @action
+  getPokemon({int index}){
+    return _pokeAPI.pokemon[index];
+  }
+
+  @action
+  Widget getImage({String numero}) {
+    return CachedNetworkImage(
+      placeholder: (context, url) => new Container(
+        color: Colors.transparent,
+      ),
+      imageUrl:
+      'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$numero.png',
+    );
+  }
+
 
   Future<PokeAPI> loadPokeAPI() async {
     try {
@@ -33,4 +53,6 @@ abstract class _PokeApiStoreBase with Store {
       return null;
     }
   }
+
+
 }
